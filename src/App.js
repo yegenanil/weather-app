e016c892688e8e983941bce8658e962b2583428a
anil yegen <ygnanil@gmail.com>
2022-05-12 19:17:56 +0300

    add: some files and edited comp.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DetailCard from './components/DetailCard';
 import Header from './components/Header';
 import SummaryCard from './components/SummaryCard';
 
+
 function App() {
-  const API_KEY = process.env.REACT_APP_API_KEY
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   const [noData, setNoData] = useState("No Data Yet");
-  const [searchTerm, setSearchTerm] = useState("");
   const [weatherData, setWeatherData] = useState([]);
+  const [search, setSearch] = useState('')
   const [city, setCity] = useState('Unknown location');
   const [weatherIcon, setWeatherIcon] = useState(`${process.env.REACT_APP_ICON_URL}10n@2x.png`);
 
-  const handleSubmit = () => {
-
+  const handleChange = (e) => {
+    setSearch(e.target.value)
   }
 
-  const handleChange = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeather(search);
+    setSearch('');
+  }
 
+  const getWeather = async (location) => {
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}`)
+    let data = await res.json();
+    if (data.cod != 200) {
+      setNoData('Location Not Found')
+      return
+    }
+
+    console.log(data)
+
+    setCity(`${data.city.name}, ${data.city.country}`);
+    setWeatherIcon(`${process.env.REACT_APP_ICON_URL + data.list[0].weather[0]["icon"]}@4x.png`);
   }
 
   return (
-    <div className="bg-gray-800 flex items-center justify-center w-screen h-screen py-10">
+    <div className="main-container  flex items-center justify-center w-screen h-screen py-10">
       <div className='flex w-3/4 min-h-full rounded-3xl shadow-lg m-auto bg-gray-100'>
         <div className='form-container'>
           <div className='flex items-center justify-center'>
-            <h3 className='my-auto mr-auto text-xl text-pink-800 font-bold shadow-md py-1 px-3 rounded--md bg-white bg-opacity-30'>
-              forecast</h3>
+            <h3 className='my-auto mr-auto text-xl text-pink-800 font-bold shadow-md py-1 px-3 rounded-md bg-white bg-opacity-30'>
+              Forecast</h3>
             <div className='flex p-2 text-gray-100 bg-gray-600 bg-opacity-30 rounded-lg'>
-              <i className='fa fa-map my-auto' aria--hidden="true"></i>
+              <i className='fa fa-map my-auto' aria-hidden="true"></i>
               <div className="text-right">
                 <p className='font-semibold text-sm ml-2'>{city}</p>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-items-center h-full">
+          <div className="flex flex-col items-center justify-center h-full">
             <h1 className='text-white text-2xl'>Weather Forecast App</h1>
             <hr className='h-1 bg-white w-1/4 rounded-full my-5' />
-            <form noValidate onSubmit={handleSubmit} className="flex justify-center w-full">
+            <form noValidate onSubmit={handleSubmit} className="flex justify-center  w-full">
               <input
-                className='relative rounded-xl py-2 px-3 bg-gray-300 bg-opacity-60 text-white placeholder-gray-200'
+                className='input relative rounded-xl py-2 px-3 bg-gray-300 bg-opacity-60 text-white placeholder-gray-200'
                 onChange={handleChange}
+                value={search}
                 required
               />
               <button type="submit" className='z-10'>
-                <i className='fa fa-search text-white ml-10 border-1 my-auto z-10 cursor-pointer p-3'
-                  aria-hidden="true"
-                  onClick={() => {
-                    navigator.geolocation.getCurrentPosition(null)
-                  }}></i>
+                <i className='search-icon fa fa-search text-white ml-10 border-1 my-auto z-10 cursor-pointer p-3'></i>
               </button>
             </form>
           </div>
@@ -64,7 +78,7 @@ function App() {
               <>
                 <h1 className='text-5xl text-gray-800 mt-auto mb-4'>Today</h1>
                 <DetailCard weather_icon={weatherIcon} data={weatherData} />
-                <h1 className='text-3xl text-gray-600 mb-4 mt-10'>More on {/* {City} */}</h1>
+                <h1 className='text-3xl text-gray-600 mb-4 mt-10'>More on {city}</h1>
                 <ul className='grid grid-cols-2 gap-2'>
                   {weatherData.list.map((days, index) => {
                     if (index > 0) {
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <ul className='flex ml-auto w-full font-bold'>
-        <li className='text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400 cursor-pointer'>Weather</li>
-        <li className='text-xs text-gray-800 mr-6 alert-notice cursor-pointer border-b-2 hover:border-green-400'>Alerts</li>
-        <li className='text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400'>Map</li>
-        <li className='text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400'>Satelite</li>
-        <li className='text-xs text-gray-800 cursor-pointer border-b-2 hover:border-green-400'>News</li>
+    <ul className='flex w-full font-bold '>
+        <li className='header-container text-xs text-gray-800  mr-6 border-b-2  cursor-pointer hover:border-green-400'>Weather</li>
     </ul>
   )
 }
